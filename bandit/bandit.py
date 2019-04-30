163df3aa229772e490fbdbff27381616a1136757
rapha <raphael.ottoni@hekima.com>
2019-04-30 12:11:44 -0300

    adding acumulative variables and some bug fixing

--- bandit/bandit.py
+++ bandit/bandit.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+from collections import defaultdict
 
 class Bandit:
     """
@@ -7,7 +8,7 @@ class Bandit:
 
     """
 
-    def __init__(self, means, STDs, stationary = True, uses_mean_before_change = 30, seed = 42):
+    def __init__(self, means, STDs, non_stationary = False, uses_mean_before_change = 30, seed = 42):
         '''
         Class constructor
 
@@ -17,7 +18,7 @@ class Bandit:
 
             STDs (list of floats): list of all arms standard deviations (Gaussian modeling)
 
-            Statitionary (Boolean): Either or not the gaussian modelings will be stationary or the means will change
+            non-stationary (Boolean): if True the mean of the gaussian reward modeling will change, it will remain stationary otherwise
 
             uses_before_change (integer): The mean of how many times the arm will be used before its gaussian reward modeling change.
 
@@ -26,12 +27,15 @@ class Bandit:
 
         if len(means) != len(STDs):
             raise Exception("the number of arms indicated by the list of reward means is not the same as indicated as the list of standard-deviation of those means")
-
+    
         self.means = means
         self.STDs  = STDs
-        self.stationary = stationary
+        self.stationary = False if non_stationary else True
         self.uses_mean_before_change = uses_mean_before_change
         self.arm_uses = len(means) * [0]
+        self.total_arms_uses = 0
+        self.arms_rewards = defaultdict(list)
+        self.acumulative_reward = 0
         
     def __str__ (self):
         '''
@@ -39,13 +43,14 @@ class Bandit:
 
         '''
         number_of_arms = len(self.means)
-        arms_type = "stationary" if self.stationary else "dynamic"
+        arms_type = "STATIONARY" if self.stationary else "DYNAMIC"
 
         return "Mult-armed bandit with " + str(number_of_arms) + " "  + arms_type  +  " arms.\n" +\
                "Reward_means = " + str(self.means) +  "\n" +\
                "Rewards_stds = " + str(self.STDs) + "\n" +\
                "Arm mean uses before change reward mean = " + str(self.uses_mean_before_change) + "\n" +\
-               "Uses of each arm " + str(self.arm_uses) + "\n"
+               "Uses of each arm " + str(self.arm_uses) + "\n" +\
+               "Accumalated reward with " + str(self.total_arms_uses) + " now:" + str(self.acumulative_reward) + "\n"
 
     def pull_arm(self, arm_id):
         '''
@@ -67,9 +72,13 @@ class Bandit:
 
         # count the use of this arm
         self.arm_uses[arm_id] += 1
+        self.acumulative_reward += reward
+
+        # Save history
+        self.arms_rewards[arm_id].append(reward)
 
         # check if the mean should change
-        if self.stationary and self.arm_uses[arm_id] % self.uses_mean_before_change == 0:
+        if not self.stationary and self.arm_uses[arm_id] % self.uses_mean_before_change == 0:
             # changes the mean and the standard deviation
             self.means[arm_id] = np.random.normal(self.means[arm_id], 0.3, 1)[0]
             self.STDs[arm_id] = np.random.normal(self.STDs[arm_id], 0.3, 1)[0]
@@ -88,7 +97,7 @@ def main():
                         help="list of all arms reward means")
     parser.add_argument('-stds', '--stand_deviations', type=float, nargs='+', required=True,
                         help="list of all arms standard deviations")
-    parser.add_argument('-s', '--stationary', action='store_true',
+    parser.add_argument('-ns', '--non-stationary', action='store_true',
                         help="if set true, the mean rewards of each arm will change over usage.")
     parser.add_argument('-umbc', '--uses_mean_before_change', type=int,
                         help="The mean number of times a arm will be used before change its reward function", default = 30)
@@ -96,7 +105,7 @@ def main():
     # initialize parser
     args = parser.parse_args()
 
-    bandit = Bandit(args.means, args.stand_deviations, args.stationary, args.uses_mean_before_change)
+    bandit = Bandit(args.means, args.stand_deviations, args.non_stationary, args.uses_mean_before_change)
     print(bandit)
     print(bandit.pull_arm(0))
     print(bandit.pull_arm(0))
